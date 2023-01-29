9b6cac736a6671ae66ced942494ec41a1400a163
Ivan-mwaura <94759966+Ivan-mwaura@users.noreply.github.com>
2023-01-29 12:58:52 +0300

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import "../Pages/style.scss"
-import { Link ,Routes,Route} from "react-router-dom";
-import Profile from "../Components/ProfilePage";
-import About from "../Components/About";
-import Projects from "../Components/Projects"
-import Contact from "../Components/Contact";
-import Resume from "../Components/Resume"
+import { Link } from "react-router-dom";
 import {Justify} from "react-bootstrap-icons"
-import {X} from "react-bootstrap-icons";
+import {X} from "react-bootstrap-icons"
 import useCollapse from "react-collapsed";
-import {Navigate} from "react-router-dom";
 
 const Navbar = () =>{
    
-    const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
+    
+    const { getCollapseProps, getToggleProps, isExpanded,setExpanded} = useCollapse();
+
+    const handleMenuClick = ()=>{
+       setExpanded(!isExpanded)
+    }
+  
     return(
     <div className="Navbar--container">
         <nav className="Navbar">  
@@ -24,41 +24,37 @@ const Navbar = () =>{
                 >
                     {isExpanded ? <X
                     size={40}
-                    color ="white"
+                    color ="#64ffda"
                     />: <Justify
                     size={40}
                     color ="#64ffda"
 
                 />}</button> 
               </div >
-              <div{...getCollapseProps()}>
+              <div {...getCollapseProps()}>
                     <div className="content">
+
+
                         <ol>
-                            <li><Navigate to="/about"/>About</li>
-                            <li><Link  to ="/profile" className="link">Profile</Link></li>
-                            <li><Link Nav ="/contact" className="link">Contact</Link></li>
-                            <li><Link to = "/project" className="link">Projects</Link></li>
-                            <li><Link to = "/resume" className="link">Resume</Link></li>
+                            <li onClick={handleMenuClick}><Link to = "/about" className="link" ><span className="number">01. </span>About</Link></li>
+                            <li onClick={handleMenuClick}><Link to ="/" className="link"><span className="number">02. </span>Profile</Link></li>
+                            <li onClick={handleMenuClick}><Link to ="/contact" className="link"><span className="number">03. </span>Contact</Link></li>
+                            <li onClick={handleMenuClick}><Link to = "/project" className="link"><span className="number">04. </span>Projects</Link></li>
+                            <li onClick={handleMenuClick}><Link to = "/resume" className="link"><span className="number">05. </span>Resume</Link></li>
                         </ol>
                     </div>
               </div>
 
            </div>
              
-            <button><Link to = "/resume" className="link">Resume</Link></button>
-            <button><Link to = "/project" className="link">Projects</Link></button>
-            <button><Link to ="/contact" className="link">Contact</Link></button>
-            <button><Link to ="/profile" className="link">Profile</Link></button>
-            <button><Link to ="/about" className="link">About</Link></button>
-   
+            <button><Link to = "/resume" className="link-button">Resume</Link></button>
+            <button><Link to = "/project" className="link"><span className="number">04. </span>Projects</Link></button>
+            <button><Link to ="/contact" className="link"><span className="number">03. </span>Contact</Link></button>
+            <button><Link to ="/about" className="link"><span className="number">02. </span>About</Link></button>
+            <button><Link to ="/" className="link"><span className="number">01. </span>Profile</Link></button>
+            
         </nav>
-        <Routes>
-           <Route path="/profile" element ={<Profile/>}/>
-           <Route path="/about" element = {<About/>}/>
-           <Route path="/contact" element ={<Contact/>}/>
-           <Route path ="/project" element ={<Projects/>}/>
-           <Route path = "/resume" element ={<Resume/>}/>
-        </Routes>
+       
     </div>
     )
 }
--- src/Components/SidebarProfile.jsx
+++ src/Components/SidebarProfile.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Pages/style.scss"
 import { Github } from "react-bootstrap-icons";
 import {Linkedin} from "react-bootstrap-icons"
@@ -8,47 +8,76 @@ import {Discord} from "react-bootstrap-icons"
 
 
 const SidebarProfile= () =>{
+    const [hoveredIcon, setHoveredIcon] = useState(null)
+    const [rotate,setRotate] = useState(false)
     return(
         <div className="sideprofile--container">
             <div className="profile">
                 <img src ={"/Img/huskie_5.jfif"} className="profile-image" alt = {"missing"}/>
-                <pre>Ivan Mwaura</pre>
+                    <pre 
+                    onMouseEnter={() => {setRotate(true)}}
+                    onMouseLeave ={() => {setRotate(false)}}
+                    className={rotate ? "rotate" : ""}>Ivan Mwaura</pre>
             </div>
             <div className="icons" >
+                <div
+                onMouseEnter = {()=>{setHoveredIcon("github")}}
+                onMouseLeave = {()=> {setHoveredIcon(null)}}
+                >
                 <Github
                     size={30}
-                    color ="white"
-                    className="icon"
+                    color = {hoveredIcon === "github" ? "#64ffda" : "white"}
+                    className = "icon"
                 />
+                </div>
                 <br/>
                 <br/>
-                
+                <div
+                  onMouseEnter = {()=>{setHoveredIcon("linkedin")}}
+                  onMouseLeave = {()=> {setHoveredIcon(null)}}
+                >
                 <Linkedin 
                     size={30}
-                    color ="white"
+                    color = {hoveredIcon === "linkedin" ? "#64ffda" : "white"}
                     className="icon"
                 />
+                </div>
                 <br/>
                 <br/>
+                <div
+                 onMouseEnter = {()=>{setHoveredIcon("twitter")}}
+                 onMouseLeave = {()=> {setHoveredIcon(null)}}
+                >
                 <Twitter 
                     size={30}
-                    color ="white"
+                    color = {hoveredIcon === "twitter" ? "#64ffda" : "white"}
                     className="icon"
                 />
+                </div>
                 <br/>
                 <br/>
+                <div
+                 onMouseEnter = {()=>{setHoveredIcon("instagram")}}
+                 onMouseLeave = {()=> {setHoveredIcon(null)}}
+                >
                 <Instagram
                     size={30}
-                    color ="white"
+                    color = {hoveredIcon === "instagram" ? "#64ffda" : "white"}
                     className="icon"
                 />
+                </div>
                 <br/>
                 <br/>
+                <div
+                onMouseEnter = {()=>{setHoveredIcon("discord")}}
+                onMouseLeave = {()=> {setHoveredIcon(null)}}
+                >
                 <Discord
                     size={30}
-                    color ="white"
+                    color = {hoveredIcon === "discord" ? "#64ffda" : "white"}
                     className="icon"
                 />
+                </div>
             </div>
         </div>
     )
